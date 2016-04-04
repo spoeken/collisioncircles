cad8f5f5ac1e8d9c6d31c0b048b2d003ce038a0f
Mathias Stav <mathias@feed.no>
2016-04-04 12:19:55 +0200

    static sphere

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var THREE = require('three');
 
-var SEPARATION = 100, AMOUNTX = 100, AMOUNTY = 100;
+var SEPARATION = 100, AMOUNTX = 20, AMOUNTY = 20;
 var container, stats;
 var camera, scene, renderer;
 var particles, particle, count = 0;
@@ -34,8 +34,17 @@ function init() {
 			particle = particles[ i ++ ] = new THREE.Sprite( material );
 			// particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
 			// particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 );
-			particle.position.x = 0;
-			particle.position.z = 0;
+			var x = -1 + Math.random() * 2;
+			var y = -1 + Math.random() * 2;
+			var z = -1 + Math.random() * 2;
+			var d = 1 / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+			x *= d;
+			y *= d;
+			z *= d;
+
+			particle.position.x = x * 1000;
+			particle.position.y = y * 1000;
+			particle.position.z = z * 1000;
 			particle.scale.x = particle.scale.y = 30;
 			scene.add( particle );
 		}
@@ -94,10 +103,10 @@ function render() {
 	var i = 0;
 	for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
 		for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-			particle = particles[ i++ ];
-			particle.position.x += ( Math.sin( ( iy + count ) * 0.1 ) * (count/10) );
-			particle.position.z += ( Math.cos( ( iy + count ) * 0.1 ) * (count/10) );
-			particle.position.y += ( Math.cos( ( ix + count ) * 0.1 ) * Math.sin( ( iy + count ) * 0.1 ) * (count/10));
+			// particle = particles[ i++ ];
+			// particle.position.x += ( Math.sin( ( iy + count ) * 0.1 ) * (count/10) );
+			// particle.position.z += ( Math.cos( ( iy + count ) * 0.1 ) * (count/10) );
+			// particle.position.y += ( Math.cos( ( ix + count ) * 0.1 ) * Math.sin( ( iy + count ) * 0.1 ) * (count/10));
 			// particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 8 +
 			// 	( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 8;
 		}
